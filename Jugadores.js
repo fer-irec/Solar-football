b197fe7e44305f65ae31b8cf2887303e80e909f6
fer-irec <107618579+fer-irec@users.noreply.github.com>
2025-05-11 13:10:16 +0200

    20 jugadores

--- Jugadores.js
+++ Jugadores.js
@@ -4,7 +4,22 @@ const jugadores = [
     { nombre: "Luis Torres", ataque: 3.2, defensa: 2.9 },
     { nombre: "Andrés Pérez", ataque: 5.0, defensa: 4.7 },
     { nombre: "José Ramírez", ataque: 2.1, defensa: 4.3 },
-    { nombre: "Marco Díaz", ataque: 3.9, defensa: 3.5 }
+    { nombre: "Marco Díaz", ataque: 3.9, defensa: 3.5 },
+    { nombre: "Javier López", ataque: 2.7, defensa: 3.0 },
+    { nombre: "Miguel Romero", ataque: 3.5, defensa: 4.2 },
+    { nombre: "Pedro Sánchez", ataque: 4.1, defensa: 2.8 },
+    { nombre: "Raúl Ortega", ataque: 2.9, defensa: 3.6 },
+    { nombre: "Hugo Díaz", ataque: 3.3, defensa: 3.9 },
+    { nombre: "Álvaro Ruiz", ataque: 4.4, defensa: 4.1 },
+    { nombre: "Nicolás Vera", ataque: 2.6, defensa: 2.7 },
+    { nombre: "Esteban Ríos", ataque: 3.8, defensa: 3.4 },
+    { nombre: "Iván Morales", ataque: 4.6, defensa: 2.5 },
+    { nombre: "Tomás León", ataque: 3.1, defensa: 3.7 },
+    { nombre: "Diego Benítez", ataque: 2.3, defensa: 3.8 },
+    { nombre: "Sebastián Aguilar", ataque: 3.0, defensa: 4.0 },
+    { nombre: "Bruno Vargas", ataque: 4.2, defensa: 3.1 },
+    { nombre: "Cristian Muñoz", ataque: 2.8, defensa: 3.2 },
+    { nombre: "Gabriel Reyes", ataque: 3.6, defensa: 4.5 }
   ];
   
   // Calcula media limitada entre 0 y 5
