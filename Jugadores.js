b29d17b5d4268bd59cc62c731966677a0a9c4694
fer-irec <107618579+fer-irec@users.noreply.github.com>
2025-06-23 16:57:02 +0200

    Nueva columna de puntualidad

--- Jugadores.js
+++ Jugadores.js
@@ -1,58 +1,58 @@
 // Lista de jugadores (solo ataque y defensa)
 const jugadores = [
-  { nombre: "Ale", ataque: 2.60, defensa: 2.56 },
-  { nombre: "Fer", ataque: 3.39, defensa: 3.10 },
-  { nombre: "Jacob", ataque: 4.17, defensa: 3.56 },
-  { nombre: "Min", ataque: 1.91, defensa: 1.90 },
-  { nombre: "Damian", ataque: 4.63, defensa: 4.02 },
-  { nombre: "Mario", ataque: 1.78, defensa: 2.29 },
-  { nombre: "Mirko", ataque: 3.69, defensa: 4.01 },
-  { nombre: "Queco (GK)", ataque: 1.55, defensa: 4.47 },
-  { nombre: "Oriol (GK)", ataque: 1.34, defensa: 4.30 },
-  { nombre: "Abel", ataque: 3.05, defensa: 2.95 },
-  { nombre: "Arnau", ataque: 4.37, defensa: 3.86 },
-  { nombre: "Nicolo", ataque: 3.45, defensa: 3.72 },
-  { nombre: "Merino", ataque: 2.89, defensa: 3.62 },
-  { nombre: "Liya", ataque: 3.49, defensa: 3.39 },
-  { nombre: "Yuancai", ataque: 1.55, defensa: 1.37 },
-  { nombre: "Jon", ataque: 2.72, defensa: 2.48 },
-  { nombre: "Peña", ataque: 2.68, defensa: 2.72 },
-  { nombre: "Manu", ataque: 4.48, defensa: 4.16 },
-  { nombre: "Vito", ataque: 3.64, defensa: 4.43 },
-  { nombre: "Alex Jimenez", ataque: 4.64, defensa: 4.47 },
-  { nombre: "Andrea Maioli", ataque: 4.63, defensa: 4.53 },
-  { nombre: "Outman", ataque: 4.45, defensa: 4.90 },
-  { nombre: "Harris", ataque: 2.36, defensa: 2.16 },
-  { nombre: "Sergio Ramos", ataque: 3.97, defensa: 3.53 },
-  { nombre: "Tolga", ataque: 3.40, defensa: 2.80 },
-  { nombre: "Amadou", ataque: 3.35, defensa: 3.15 },
-  { nombre: "Steven", ataque: 3.04, defensa: 3.08 },
-  { nombre: "Ricard", ataque: 2.33, defensa: 2.38 },
-  { nombre: "David Rovira", ataque: 2.98, defensa: 2.96 },
-  { nombre: "Gustavo Madrigal", ataque: 2.91, defensa: 2.96 },
-  { nombre: "Diego", ataque: 1.98, defensa: 2.37 },
-  { nombre: "Andrea Aroldi", ataque: 2.17, defensa: 2.01 },
-  { nombre: "Vitor", ataque: 2.77, defensa: 2.77 },
-  { nombre: "Mario Lecce", ataque: 3.37, defensa: 3.47 },
-  { nombre: "Jeff", ataque: 3.87, defensa: 3.37 },
-  { nombre: "Payno", ataque: 3.20, defensa: 2.90 },
-  { nombre: "Kevin", ataque: 4.64, defensa: 4.27 },
-  { nombre: "Fabien", ataque: 3.13, defensa: 3.13 },
-  { nombre: "Romain", ataque: 4.52, defensa: 4.45 },
-  { nombre: "Maykel", ataque: 3.10, defensa: 3.04 },
-  { nombre: "Alex Lopez", ataque: 3.50, defensa: 3.35 },
-  { nombre: "Buda", ataque: 3.58, defensa: 3.88 },
-  { nombre: "Massi", ataque: 4.13, defensa: 4.23 },
-  { nombre: "Trompia", ataque: 4.67, defensa: 4.40 },
-  { nombre: "Treppo", ataque: 3.83, defensa: 3.67 },
-  { nombre: "Lori", ataque: 3.57, defensa: 2.77 },
-  { nombre: "Tobi", ataque: 2.52, defensa: 3.60 },
-  { nombre: "Dominic", ataque: 3.30, defensa: 3.0 },
-  { nombre: "Rolando", ataque: 2.4, defensa: 2.75 },
-  { nombre: "Tomas", ataque: 3.5, defensa: 3.2 },
-  { nombre: "Visitor 1 (2.5)", ataque: 2.5, defensa: 2.5 },
-  { nombre: "Visitor 2( 2.5)", ataque: 2.5, defensa: 2.5 },
-  { nombre: "Visitor 3 (3)", ataque: 3, defensa: 3 }
+  { nombre: "Ale", ataque: 2.60, defensa: 2.56, puntualidad: 3},
+  { nombre: "Fer", ataque: 3.39, defensa: 3.10, puntualidad: 3},
+  { nombre: "Jacob", ataque: 4.17, defensa: 3.56, puntualidad: 3 },
+  { nombre: "Min", ataque: 1.91, defensa: 1.90, puntualidad: 3 },
+  { nombre: "Damian", ataque: 4.63, defensa: 4.02, puntualidad: 3 },
+  { nombre: "Mario", ataque: 1.78, defensa: 2.29, puntualidad: 3 },
+  { nombre: "Mirko", ataque: 3.69, defensa: 4.01, puntualidad: 3 },
+  { nombre: "Queco (GK)", ataque: 1.55, defensa: 4.47, puntualidad: 3 },
+  { nombre: "Oriol (GK)", ataque: 1.34, defensa: 4.30, puntualidad: 3 },
+  { nombre: "Abel", ataque: 3.05, defensa: 2.95, puntualidad: 3 },
+  { nombre: "Arnau", ataque: 4.37, defensa: 3.86, puntualidad: 3 },
+  { nombre: "Nicolo", ataque: 3.45, defensa: 3.72, puntualidad: 3 },
+  { nombre: "Merino", ataque: 2.89, defensa: 3.62, puntualidad: 3 },
+  { nombre: "Liya", ataque: 3.49, defensa: 3.39, puntualidad: 3 },
+  { nombre: "Yuancai", ataque: 1.55, defensa: 1.37, puntualidad: 3 },
+  { nombre: "Jon", ataque: 2.72, defensa: 2.48, puntualidad: 3 },
+  { nombre: "Peña", ataque: 2.68, defensa: 2.72, puntualidad: 3 },
+  { nombre: "Manu", ataque: 4.48, defensa: 4.16, puntualidad: 3 },
+  { nombre: "Vito", ataque: 3.64, defensa: 4.43, puntualidad: 3 },
+  { nombre: "Alex Jimenez", ataque: 4.64, defensa: 4.47, puntualidad: 3 },
+  { nombre: "Andrea Maioli", ataque: 4.63, defensa: 4.53, puntualidad: 3 },
+  { nombre: "Outman", ataque: 4.45, defensa: 4.90, puntualidad: 3 },
+  { nombre: "Harris", ataque: 2.36, defensa: 2.16, puntualidad: 3 },
+  { nombre: "Sergio Ramos", ataque: 3.97, defensa: 3.53, puntualidad: 3 },
+  { nombre: "Tolga", ataque: 3.40, defensa: 2.80, puntualidad: 3 },
+  { nombre: "Amadou", ataque: 3.35, defensa: 3.15, puntualidad: 3 },
+  { nombre: "Steven", ataque: 3.04, defensa: 3.08, puntualidad: 3 },
+  { nombre: "Ricard", ataque: 2.33, defensa: 2.38, puntualidad: 3 },
+  { nombre: "David Rovira", ataque: 2.98, defensa: 2.96, puntualidad: 3 },
+  { nombre: "Gustavo Madrigal", ataque: 2.91, defensa: 2.96, puntualidad: 3 },
+  { nombre: "Diego", ataque: 1.98, defensa: 2.37, puntualidad: 3 },
+  { nombre: "Andrea Aroldi", ataque: 2.17, defensa: 2.01, puntualidad: 3 },
+  { nombre: "Vitor", ataque: 2.77, defensa: 2.77, puntualidad: 3 },
+  { nombre: "Mario Lecce", ataque: 3.37, defensa: 3.47, puntualidad: 3 },
+  { nombre: "Jeff", ataque: 3.87, defensa: 3.37, puntualidad: 3 },
+  { nombre: "Payno", ataque: 3.20, defensa: 2.90, puntualidad: 3 },
+  { nombre: "Kevin", ataque: 4.64, defensa: 4.27, puntualidad: 3 },
+  { nombre: "Fabien", ataque: 3.13, defensa: 3.13, puntualidad: 3 },
+  { nombre: "Romain", ataque: 4.52, defensa: 4.45, puntualidad: 3 },
+  { nombre: "Maykel", ataque: 3.10, defensa: 3.04, puntualidad: 3 },
+  { nombre: "Alex Lopez", ataque: 3.50, defensa: 3.35, puntualidad: 3 },
+  { nombre: "Buda", ataque: 3.58, defensa: 3.88, puntualidad: 3 },
+  { nombre: "Massi", ataque: 4.13, defensa: 4.23, puntualidad: 3 },
+  { nombre: "Trompia", ataque: 4.67, defensa: 4.40, puntualidad: 3 },
+  { nombre: "Treppo", ataque: 3.83, defensa: 3.67, puntualidad: 3 },
+  { nombre: "Lori", ataque: 3.57, defensa: 2.77, puntualidad: 3 },
+  { nombre: "Tobi", ataque: 2.52, defensa: 3.60, puntualidad: 3 },
+  { nombre: "Dominic", ataque: 3.30, defensa: 3.0, puntualidad: 3 },
+  { nombre: "Rolando", ataque: 2.4, defensa: 2.75, puntualidad: 3 },
+  { nombre: "Tomas", ataque: 3.5, defensa: 3.2, puntualidad: 3 },
+  { nombre: "Visitor 1 (2.5)", ataque: 2.5, defensa: 2.5, puntualidad: 3 },
+  { nombre: "Visitor 2( 2.5)", ataque: 2.5, defensa: 2.5, puntualidad: 3},
+  { nombre: "Visitor 3 (3)", ataque: 3, defensa: 3, puntualidad: 3 }
 ];
 
 function calcularMedia(j) {
@@ -168,6 +168,7 @@ function mostrarTabla() {
       <td><span class="${colorClase(media)}">${media}</span></td>
       <td class="fifa"><span class="${colorFifa(fifa)}">${fifa}</span></td>
       <td class="stars">${estrellasHTML}</td>
+      <td><span class="${colorClase(j.puntualidad)}">${j.puntualidad}</span></td>
     </tr>`;
     tbody.insertAdjacentHTML("beforeend", fila);
   });
@@ -330,7 +331,9 @@ document.addEventListener("DOMContentLoaded", () => {
   mostrarTabla();
 
   // Ordenamiento de columnas de la tabla de jugadores
-  const columnas = ["nombre", "ataque", "defensa", "media", "fifa"];
+  //const columnas = ["nombre", "ataque", "defensa", "media", "fifa"];
+  const columnas = ["nombre", "ataque", "defensa", "media", "fifa", "puntualidad"];
+
   document.querySelectorAll("#tabla-jugadores thead th").forEach((th, index) => {
     const columna = columnas[index];
     if (columna) {
